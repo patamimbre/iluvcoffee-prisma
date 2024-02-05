dc9edc65fc83deceb92e2a0b1a5728e6d271e88f
Germán Castro <germancastromontes@gmail.com>
2024-02-05 20:18:15 +0100

    feat: coffees module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,15 @@ import { LoggerModule } from "@core/logger/logger.module";
 
 import { UserModule } from "@contexts/users/user.module";
 
+import { CoffeesModule } from "./contexts/coffees/coffees.module";
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
     LoggerModule,
     HealthModule,
     UserModule,
+    CoffeesModule,
   ],
 })
 export class AppModule {}
